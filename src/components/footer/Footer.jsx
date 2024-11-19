327fde8683f565f8fa2af801f4fc890b36622417
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2024-11-20 00:53:41 +0400

    add fb to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-import instagram from "../../img/icons/instagram.svg";
-import twitter from "../../img/icons/twitter.svg";
+import facebook from "../../img/icons/facebook.svg";
 import linkedIn from "../../img/icons/linkedIn.svg";
 import gitHub from "../../img/icons/gitHub.svg";
 
@@ -12,17 +11,22 @@ const Footer = () => {
         <div className="footer__wrapper">
           <ul className="social">
             <li className="social__item">
-              <a href="#!">
-                <img src={twitter} alt="Link" />
+              <a
+                href="https://www.facebook.com/buxa.buxa.7/"
+                target="_blank"
+                without
+                rel="noreferrer"
+              >
+                <img src={facebook} alt="Link" />
               </a>
             </li>
             <li className="social__item">
-              <a href="#!">
-                <img src={instagram} alt="Link" />
-              </a>
-            </li>
-            <li className="social__item">
-              <a href="https://github.com/TBukhaidze" target="_blanc">
+              <a
+                href="https://github.com/TBukhaidze"
+                target="_blank"
+                without
+                rel="noreferrer"
+              >
                 <img src={gitHub} alt="Link" />
               </a>
             </li>
