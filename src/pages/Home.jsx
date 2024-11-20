c13fa2e1ded92a9692dfb9e12263922d596f38ff
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2024-11-21 00:54:02 +0400

    added some icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,17 @@ import React from "react";
 
 import Header from "../components/header/Header";
 
+import html from "../img/icons/html.svg";
+import css from "../img/icons/css.svg";
+import bootstrap from "../img/icons/bootstrap.svg";
+import npm from "../img/icons/npm.svg";
+import webpack from "../img/icons/webpack.svg";
+import js from "../img/icons/js.svg";
+import react from "../img/icons/react.svg";
+import redux from "../img/icons/redux.svg";
+import node from "../img/icons/node.svg";
+import mysql from "../img/icons/mysql.svg";
+
 const Home = () => {
   return (
     <>
@@ -12,14 +23,55 @@ const Home = () => {
           <ul className="content-list">
             <li className="content-list__item">
               <h2 className="title-2">Frontend</h2>
-              <p>
-                JavaScript, TypeScript, ReactJS, Redux, Redux-toolkit, HTML,
-                CSS, NPM, SCSS, BootStrap, MaterialUI, Yarn, StyledComponents
-              </p>
+              <ul className="social" style={{ marginBottom: "30px" }}>
+                <li className="social__item">
+                  <img src={html} alt="Link" />
+                  <p>HTML</p>
+                </li>
+                <li className="social__item">
+                  <img src={css} alt="Link" />
+                  <p>CSS,SCSS</p>
+                </li>
+                <li className="social__item">
+                  <img src={bootstrap} alt="Link" />
+                  <p>Bootstrap</p>
+                </li>
+                <li className="social__item">
+                  <img src={npm} alt="Link" />
+                  <p>NPM</p>
+                </li>
+              </ul>
+              <ul className="social">
+                <li className="social__item">
+                  <img src={webpack} alt="Link" />
+                  <p>Webpack</p>
+                </li>
+                <li className="social__item">
+                  <img src={js} alt="Link" />
+                  <p>JS</p>
+                </li>
+                <li className="social__item">
+                  <img src={react} alt="Link" />
+                  <p>ReactJS</p>
+                </li>
+                <li className="social__item">
+                  <img src={redux} alt="Link" />
+                  <p>Redux-toolkit</p>
+                </li>
+              </ul>
             </li>
             <li className="content-list__item">
               <h2 className="title-2">Backend</h2>
-              <p>NodeJS,asdasd MySQL, MongoDB, PHP, Laravel</p>
+              <ul className="social">
+                <li className="social__item">
+                  <img src={node} alt="Link" />
+                  <p>NodeJS</p>
+                </li>
+                <li className="social__item">
+                  <img src={mysql} alt="Link" />
+                  <p>MySQL</p>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
