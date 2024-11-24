629df7dc2f24cb54cdf05956ffafe03f6dfee689
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2024-11-24 20:15:26 +0400

    update contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,9 @@
+import telegram from "../img/icons/telegram.svg";
+import telegramQR from "../img/QR/telegramQR.jpg";
+
+import whatsapp from "../img/icons/whatsapp.svg";
+import whatsappQR from "../img/QR/whatsappQR.jpg";
+
 const Contact = () => {
   return (
     <main className="section">
@@ -9,19 +15,58 @@ const Contact = () => {
             <h2 className="title-2">Location</h2>
             <p>Tbilisi, Georgia</p>
           </li>
+
           <li className="content-list__item">
-            <h2 className="title-2">Telegram / WhatsApp</h2>
-            <p>
-              <a href="tel:+79051234567">+(995) 598 55 82 33</a>
-            </p>
+            <div style={{ display: "flex" }}>
+              <div style={{ margin: "20px" }}>
+                <h2 className="title-2">
+                  <img
+                    src={telegram}
+                    alt="TelegramIcon"
+                    style={{ marginRight: "10px" }}
+                  />
+                  Telegram
+                </h2>
+                <img
+                  src={telegramQR}
+                  alt="telegramQR"
+                  style={{ width: "175px" }}
+                />
+              </div>
+
+              <div style={{ margin: "20px" }}>
+                <h2 className="title-2">
+                  <img
+                    src={whatsapp}
+                    alt="WhatsAppIcon"
+                    style={{ marginRight: "10px" }}
+                  />
+                  Whatsapp
+                </h2>
+                <img
+                  src={whatsappQR}
+                  alt="whatsappQR"
+                  style={{ width: "175px" }}
+                />
+              </div>
+            </div>
           </li>
+
+          <li className="content-list__item"></li>
+
           <li className="content-list__item">
-            <h2 className="title-2">Email</h2>
-            <p>
-              <a href="mailto:webdev@protonmail.com">
-                bukhaidze.temo@gmail.com
-              </a>
-            </p>
+            <h2 className="title-2">Email/Number</h2>
+            <div>
+              <p>
+                <a href="mailto:bukhaidze.temo@gmail.com">
+                  bukhaidze.temo@gmail.com
+                </a>
+              </p>
+
+              <p>
+                <a href="tel:+995598558233">+(995) 598 55 82 33</a>
+              </p>
+            </div>
           </li>
         </ul>
       </div>
