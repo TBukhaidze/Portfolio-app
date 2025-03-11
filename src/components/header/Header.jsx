c28f93fc409078e58b0a0d2c1aeea533e219e09e
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-03-11 21:54:12 +0400

    add typewriter effect to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CV from "../cv/TB-CV.pdf";
+import Typewriter from "typewriter-effect";
 
 const Header = () => {
+  const [showSecondTypewriter, setShowSecondTypewriter] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowSecondTypewriter(true);
+    }, 2500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <header className="header">
       <div className="header__wrapper">
         <h1 className="header__title">
           <strong>
-            Hi, my name is <br /> <em>Temuri Bukhaidze</em>
+            Hi, my name is <br />
+            <em>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString("Temuri Bukhaidze").start();
+                }}
+                options={{
+                  cursor: ".",
+                }}
+              />
+            </em>
           </strong>
-          <br />a frontend developer
+          {showSecondTypewriter && (
+            <Typewriter
+              options={{
+                strings: ["Frontend Developer", "React/Next.js Developer"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          )}
         </h1>
         <div className="header__text">
           <p>with passion for learning and creating.</p>
