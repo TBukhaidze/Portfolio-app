1fe842982bd5d5da0ca239c216b472b74b2c9adc
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-04-27 17:34:41 +0400

    optimize Home component with dynamic skills rendering

---
+++ src/helpers/skilsList.js
@@ -0,0 +1,24 @@
+export const skilsList = [
+  {
+    category: "Frontend",
+    items: [
+      [
+        { icon: "html", label: "HTML" },
+        { icon: "css", label: "CSS/SCSS" },
+        { icon: "bootstrap", label: "Bootstrap", className: "mr-5" },
+        { icon: "tailwind", label: "Tailwind" },
+      ],
+      [
+        { icon: "git", label: "Git", className: "mr-5" },
+        { icon: "js", label: "JS", className: "mr-10" },
+        { icon: "ts", label: "TS", className: "mr-5" },
+        { icon: "react", label: "ReactJS" },
+      ],
+      [
+        { icon: "redux", label: "Redux-toolkit" },
+        { icon: "next", label: "Next.js", className: "mr-15" },
+        { icon: "mongoDB", label: "MongoDB", className: "mr-15" },
+      ],
+    ],
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,72 +1,38 @@
 import React from "react";
-
 import Header from "../components/header/Header";
 import { icons } from "../helpers/icons";
+import { skilsList } from "../helpers/skilsList";
 
 const Home = () => {
   return (
     <>
       <Header />
-
       <main className="section">
         <div className="container">
           <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <ul className="social" style={{ marginBottom: "30px" }}>
-                <li className="social__item">
-                  <img src={icons.html} alt="Link" />
-                  <p>HTML</p>
-                </li>
-                <li className="social__item">
-                  <img src={icons.css} alt="Link" />
-                  <p>CSS/SCSS</p>
-                </li>
-                <li className="social__item" style={{ marginRight: "5px" }}>
-                  <img src={icons.bootstrap} alt="Link" />
-                  <p>Bootstrap</p>
-                </li>
-                <li className="social__item">
-                  <img src={icons.tailwind} alt="Link" />
-                  <p>Tailwind</p>
-                </li>
-              </ul>
-              <ul className="social">
-                <li className="social__item" style={{ marginRight: "5px" }}>
-                  <img src={icons.git} alt="Link" />
-                  <p>Git</p>
-                </li>
-                <li className="social__item" style={{ marginRight: "10px" }}>
-                  <img src={icons.js} alt="Link" />
-                  <p>JS</p>
-                </li>
-                <li className="social__item" style={{ marginRight: "5px" }}>
-                  <img src={icons.ts} alt="Link" />
-                  <p>TS</p>
-                </li>
-                <li className="social__item">
-                  <img src={icons.react} alt="Link" />
-                  <p>ReactJS</p>
-                </li>
-              </ul>
-              <ul
-                className="social"
-                style={{ justifyContent: "center", marginTop: "20px" }}
-              >
-                <li className="social__item">
-                  <img src={icons.redux} alt="Link" />
-                  <p>Redux-toolkit</p>
-                </li>
-                <li className="social__item" style={{ marginRight: "15px" }}>
-                  <img src={icons.next} alt="Link" />
-                  <p>Next.js</p>
-                </li>
-                <li className="social__item" style={{ marginRight: "15px" }}>
-                  <img src={icons.mongoDB} alt="Link" />
-                  <p>MongoDB</p>
-                </li>
-              </ul>
-            </li>
+            {skilsList.map(({ category, items }, index) => (
+              <li key={index} className="content-list__item">
+                <h2 className="title-2">{category}</h2>
+                {items.map((skillGroup, groupIndex) => (
+                  <ul
+                    key={groupIndex}
+                    className={`social ${
+                      groupIndex === items.length - 1 ? "social--centered" : ""
+                    }`}
+                  >
+                    {skillGroup.map(({ icon, label, className }) => (
+                      <li
+                        key={icon}
+                        className={`social__item ${className || ""}`}
+                      >
+                        <img src={icons[icon]} alt={label} />
+                        <p>{label}</p>
+                      </li>
+                    ))}
+                  </ul>
+                ))}
+              </li>
+            ))}
           </ul>
         </div>
       </main>
