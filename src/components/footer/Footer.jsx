f7f9401771fdde26ddaa0155cfd1e0bf76246995
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-06-24 17:38:26 +0400

    added socialLinks file

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,23 +1,4 @@
-import React from "react";
-import { icons } from "../../helpers/icons";
-
-const socialLinks = [
-  {
-    href: "https://www.facebook.com/buxa.buxa.7/",
-    icon: icons.facebook,
-    alt: "Facebook",
-  },
-  {
-    href: "https://github.com/TBukhaidze",
-    icon: icons.gitHub,
-    alt: "GitHub",
-  },
-  {
-    href: "https://www.linkedin.com/in/temuri-bukhaidze-83728633a/",
-    icon: icons.linkedIn,
-    alt: "LinkedIn",
-  },
-];
+import { socialLinks } from "../../helpers/socialLinks";
 
 const year = new Date().getFullYear();
 
---
+++ src/helpers/socialLinks.js
@@ -0,0 +1,21 @@
+import { icons } from "../helpers/icons";
+
+const socialLinks = [
+  {
+    href: "https://www.facebook.com/buxa.buxa.7/",
+    icon: icons.facebook,
+    alt: "Facebook",
+  },
+  {
+    href: "https://github.com/TBukhaidze",
+    icon: icons.gitHub,
+    alt: "GitHub",
+  },
+  {
+    href: "https://www.linkedin.com/in/temuri-bukhaidze-83728633a/",
+    icon: icons.linkedIn,
+    alt: "LinkedIn",
+  },
+];
+
+export { socialLinks };
